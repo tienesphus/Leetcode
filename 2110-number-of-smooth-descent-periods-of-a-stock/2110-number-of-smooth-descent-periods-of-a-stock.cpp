091275c4ebc2c798f717df288b59663c5e8a1674
Phu N <54136676+tienesphus@users.noreply.github.com>
2022-03-03 08:52:57 +1030

    Time: 183 ms (62.90%), Space: 93.6 MB (23.91%) - LeetHub

---
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long ans = 0;
+        long long pricesSize = prices.size();
+        ans += pricesSize;
+        
+        if (pricesSize == 1)
+            return ans;
+        
+        // find all the longest non overlapping smooth periods
+        vector<long long> periodsLength;
+        int curVal = prices[0];
+        int curStreak = 1;
+        for (int i = 1; i < pricesSize; ++i) {
+            bool streakContinue = (curVal == prices[i] + 1);
+            
+            if (streakContinue)
+                curStreak++;
+            
+            if (curStreak > 1 && (!streakContinue || i == pricesSize - 1)) {
+                periodsLength.push_back(curStreak);
+                curStreak = 1;
+            }
+            
+            curVal = prices[i];
+        }
+        
+        // calculate the total number of smooth periods based on that
+        for (auto& period: periodsLength) {
+            ans += (period - 1)*(period)/2;
+        }
+        
+        // return the answer
+        return ans;
+    }
+};
+
+// find all the longest non overlapping smooth periods
+// calculate the total number of smooth periods based on that
+//
