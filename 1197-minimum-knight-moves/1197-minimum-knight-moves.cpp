c634a67b2f77470437ee86cb3bc68eaddb79e602
Phu N <54136676+tienesphus@users.noreply.github.com>
2022-03-03 12:24:10 +1030

    Time: 1328 ms (16.18%), Space: 333.7 MB (5.01%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -12,27 +12,27 @@ public:
         queue<vector<int>> q;
         q.push({300,300});
         
+        vector<int> curCel;
+        int newX, newY;
+        
         while (!q.empty()) {
-            auto curCel = q.front();
+            curCel = q.front();
             q.pop();
-            //cout << "Current cel " << curCel[0] << " " << curCel[1] << '\n'; 
+
             if (curCel[0] == x && curCel[1] == y)
                 return visited[curCel[0]][curCel[1]];
             
             for (auto& direction: directions) {
-                int newX = curCel[0] + direction[0];
-                int newY = curCel[1] + direction[1];
+                newX = curCel[0] + direction[0];
+                newY = curCel[1] + direction[1];
                 
                 if (newX < 0 || newX >= m || newY < 0 || newY >= n || visited[newX][newY] != -1)
                     continue;
                 
                 visited[newX][newY] = visited[curCel[0]][curCel[1]] + 1;
                 q.push({newX,newY});
-                
-                //cout << "visited count:: " << newX << " " << newY << " " << visited[newX][newY] << '\n';
              }
         }
-        
-        return 0;
+        return -1;
     }
 };
