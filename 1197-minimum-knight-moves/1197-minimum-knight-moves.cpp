ea77ccee08753207f262ff5c102821dac3b8a35b
Phu N <54136676+tienesphus@users.noreply.github.com>
2022-03-03 12:40:53 +1030

    Time: 634 ms (39.03%), Space: 119.4 MB (15.26%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minKnightMoves(int x, int y) {
-        vector<vector<int>> directions = {{2,1}, {1,2}, {-1,2}, {1,-2}, {-1,-2}, {-2,1}, {2,-1}, {-2,-1}};
+        vector<pair<int,int>> directions = {{2,1}, {1,2}, {-1,2}, {1,-2}, {-1,-2}, {-2,1}, {2,-1}, {-2,-1}};
         x = x + 300;
         y = y + 300;
         vector<vector<int>> visited(601, vector<int>(601, -1));
@@ -23,8 +23,8 @@ public:
                 return visited[curCel.first][curCel.second];
             
             for (auto& direction: directions) {
-                newX = curCel.first + direction[0];
-                newY = curCel.second + direction[1];
+                newX = curCel.first + direction.first;
+                newY = curCel.second + direction.second;
                 
                 if (newX < 0 || newX >= m || newY < 0 || newY >= n || visited[newX][newY] != -1)
                     continue;
