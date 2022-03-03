ccb8fd9ebe859ee347e6c8078d413cdc8294f259
Phu N <54136676+tienesphus@users.noreply.github.com>
2022-03-03 10:57:08 +1030

    Time: 100 ms (41.22%), Space: 40.3 MB (12.58%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int numTimesAllBlue(vector<int>& flips) {
-        vector<int> binary(flips.size()+1, 0);
+        vector<int> binary(flips.size(), 0);
         int pointer = 0;
         int ans = 0;
         
         for (int i = 0; i < flips.size(); i++) {
             binary[flips[i] - 1] = 1;
-            while (binary[pointer] != 0)
+            while (pointer < flips.size() && binary[pointer] != 0)
                 pointer++;
             
             if (pointer >= i+1)
