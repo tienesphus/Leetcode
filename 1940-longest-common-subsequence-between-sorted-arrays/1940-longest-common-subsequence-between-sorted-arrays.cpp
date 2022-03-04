30e1614a1b30a7f6127fedd54209123d846d4e6f
Phu N <54136676+tienesphus@users.noreply.github.com>
2022-03-04 17:59:14 +1030

    Time: 38 ms (81.51%), Space: 15.8 MB (47.90%) - LeetHub

---
+++ 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> longestCommonSubsequence(vector<vector<int>>& arrays) {
+        unordered_map<int,int> countTable;
+        int n = arrays.size();
+        
+        for (auto& array: arrays) {
+            for (auto& ele: array) {
+                auto it = countTable.find(ele);
+                
+                if (it != countTable.end())
+                    it->second += 1;
+                else {
+                    countTable.insert(make_pair(ele, 1));
+                }
+            }
+        }
+        
+        vector<int> ans;
+        for (auto& pair: countTable) {
+            if (pair.second == n)
+                ans.push_back(pair.first);
+        }
+        sort(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
+
+
