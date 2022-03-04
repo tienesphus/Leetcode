aad72446e23e589103a433e6277e8245d8c99003
Phu N <54136676+tienesphus@users.noreply.github.com>
2022-03-04 11:44:41 +1030

    Time: 4 ms (49.33%), Space: 7.4 MB (55.35%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -3,16 +3,16 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,0);
+        int ans = 0;
         
         for (int i = 2; i < n; i++) {
             bool checkArithmetic = ((nums[i] - nums[i-1]) == (nums[i-1] - nums[i-2]));
-            if (checkArithmetic)
+            if (checkArithmetic) {
                 dp[i] = dp[i-1] + 1;
+                ans += dp[i];
+            }
+
         }
-        
-        int ans = 0;
-        for (auto& ele: dp)
-            ans += ele;
         
         return ans;
     }
