72a4068317b04d6cdb3a86cf0fd35e97382edb02
Phu N <54136676+tienesphus@users.noreply.github.com>
2022-03-04 18:16:14 +1030

    Time: 36 ms (89.92%), Space: 15.7 MB (58.82%) - LeetHub

--- 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.cpp
+++ 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.cpp
@@ -6,6 +6,7 @@ public:
         
         vector<int> countTable(m+1, 0);
         vector<int> ans;
+        
         // O(n) with n is the number of elements in all the arrays
         for (auto& array: arrays) {
             for (auto& ele: array) {
@@ -16,7 +17,7 @@ public:
                     ans.push_back(ele);
             }
         }
-
+        // O(m) with m is the largest number out of all elements in arrays
         return ans;
     }
 };
